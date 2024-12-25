ba3dabbb7c26f0101663ea9e4cad0b042672f6d0
Ari <arifebres28@gmail.com>
2024-12-24 18:45:42 -0800

    - Added `Song` class with attributes: title, artist, album, year, duration, and album_art. - Implemented `__str__` method for `Song` class. - Added `Player` class inheriting from `Song` with methods: play, pause, stop, next, previous, shuffle, and repeat. - Created `PlaylistManager'

---
+++ main.py
@@ -0,0 +1,7 @@
+import Song
+
+def main():
+    print("Hello, world!")
+    # Create a new song
+    song = Song("Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975, "5:55", "album_art.jpg")
+    print(song)
