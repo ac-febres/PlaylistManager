b4520d9fe0668f87d1f6f52272133a256bdee835
Ari <arifebres28@gmail.com>
2025-01-02 20:16:17 -0800

    Added welcome message into main function. Created variable for sleep timer function to more easily change values. Created PlaylistManager.py Added commenting to main.py to outline the way the program should work.

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import Song
+# Aria Febres - Python 3.13
+# Date: 10/10/2021
+# Description: This program will create a music library for a user to input their music.
+# It will also allow the user to search for a song, artist, or album in their library.
+# The user will be able to play, pause, stop, skip, shuffle, and repeat songs in their library.
+# The user will also be able to create and delete playlists.
+# The user will be able to save their music library and playlists to a file.
+# The user will be able to load their music library and playlists from a file.
+# The user will be able to print their music library and playlists to the console.
+# The user will be able to clear their music library and playlists.
+# The user will be able to quit the program.
 
+import time
+def border():
+    print("--------------------------------------------------")
 def main():
-    print("Hello, world!")
-    # Create a new song
-    song = Song("Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975, "5:55", "album_art.jpg")
-    print(song)
+    #Timing variable to pace print statement for user
+    sleep_timing = 1
+
+    # Welcome message
+    border()
+    print("Welcome to the Music Library!"
+          "Here you can create your own music library and playlists.",
+          sep="\n")
+    time.sleep(sleep_timing)
+    print("Let's get started!")
+    time.sleep(sleep_timing)
+    print("")
+    border()
+main()
